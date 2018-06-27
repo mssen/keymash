47bca3efa0724748f1f3dbcbc83436095a876c7e
mssen <missy.danielsen@gmail.com>
2018-06-26 21:25:02 -0500

    Basic marking with Slate.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,11 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { Value } from 'slate';
+import Plain from 'slate-plain-serializer';
 
 import TextField from './editor/TextField';
 import Viewer from './viewer/Viewer';
 
-const initialValue = Value.fromJSON({
-  document: {
-    nodes: [
-      {
-        object: 'block',
-        type: 'paragraph',
-        nodes: [
-          {
-            object: 'text',
-            leaves: [
-              {
-                text: 'A line of text in a paragraph.'
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }
-});
+const initialValue = Plain.deserialize('A line of text in a paragraph.');
 
 const Container = styled.div`
   display: grid;
--- src/editor/TextField.js
+++ src/editor/TextField.js
@@ -1,14 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Editor } from 'slate-react';
+import isHotkey from 'is-hotkey';
 
 const StyledEditor = styled(Editor)`
   height: 100vh;
   border: 1px solid black;
 `;
 
+const isBoldHotKey = isHotkey('mod+b');
+
+const onKeyDown = (event, change) => {
+  if (isBoldHotKey(event)) {
+    event.preventDefault();
+    change.toggleMark('bold');
+    return true;
+  }
+  return false;
+};
+
+const renderMark = ({ mark, children }) => {
+  switch (mark.type) {
+    case 'bold':
+      return <strong>{children}</strong>;
+    default:
+      return children;
+  }
+};
+
 const TextField = ({ value, onChange }) => (
-  <StyledEditor value={value} onChange={onChange} />
+  <StyledEditor
+    value={value}
+    onChange={onChange}
+    onKeyDown={onKeyDown}
+    renderMark={renderMark}
+  />
 );
 
 export default TextField;
